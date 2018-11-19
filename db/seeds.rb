8bedb5ed3d16dd5b29ec9c62d2ab654636f594b2
Cedric Dlh <cedricdlh@gmail.com>
2018-11-19 17:48:34 +0100

    Created a dropdown simple form

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+tool = ["drill", "hammer", "screwdriver", "brush"]
+
+location = ["Berlin", "Frankfurt", "Munich", "Cologne", "Dusselforf", "Hamburg"]
