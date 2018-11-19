36fa4e4fc527cf0228351b408ea9084ec3bcecb7
ole dening <ole.dening@gmail.com>
2018-11-19 18:08:28 +0100

    validation for tools

--- app/models/tool.rb
+++ app/models/tool.rb
@@ -1,4 +1,14 @@
 class Tool < ApplicationRecord
   belongs_to :user
   has_many :bookings
+
+  validates :name, presence: true
+  validates :category, presence: true
+  validates :photo, presence: true
+  validates :location, presence: true
+  validates :price_per_day, presence: true
+
+  # CLOUDINARY
+  # mount_uploader :photo, PhotoUploader
+  # validates_presence_of :photo
 end
