0abcdaa1398b781d1069dcc8863e73e7562493a8
Cedric Dlh <cedricdlh@gmail.com>
2018-11-21 14:16:58 +0100

    created map on the index

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,8 @@
-User.create(first_name: "Admin", email: "admin@admin.com" , password: "12345678", admin: true)
-User.create(first_name: "Lucas", email: "luc_petti@hotmail.com", password: "12345678")
+User.create!(first_name: "Admin", email: "admin@admin.com" , password: "12345678", admin: true)
+User.create!(first_name: "Lucas", email: "luc_petti@hotmail.com", password: "12345678")
 
 category = ["drill", "hammer", "screwdriver", "brush"]
-location = ["Berlin", "Frankfurt", "Munich", "Cologne", "Dusselforf", "Hamburg"]
-photo = ["283333_1","106105_1","GSR_108-2-LI","656837_1","418469_1","31bham0MybL._SX355_"]
+location = ["Uhlandstraße 167 Berlin", "Charlottenstraße 1 Berlin", "Hedwig-Wachenheim-Straße 12 Berlin", "Lauterstraße 25 Berlin", "Rosenthalerstraße 27 Berlin"]
 price_per_day = ["5","6","7","8","9","10","11"]
 description = ["Its a wonderful tool- maintained by my friend Cedric",
   "Its a wonderful tool- maintained by my Ole",
@@ -15,41 +14,36 @@ Tool.create(name: "Tool1",
   available: true,
   user: User.first,
   location: location.sample,
-  photo: photo.sample,
   price_per_day: price_per_day.sample)
 
-Tool.create(name: "Tool2",
+Tool.create!(name: "Tool2",
   description: description.sample,
   category: category.sample,
   available: true,
   user: User.first,
   location: location.sample,
-  photo: photo.sample,
   price_per_day: price_per_day.sample)
 #
-Tool.create(name: "Tool3",
+Tool.create!(name: "Tool3",
   description: description.sample,
   category: category.sample,
   available: true,
   user: User.first,
   location: location.sample,
-  photo: photo.sample,
   price_per_day: price_per_day.sample)
 
-Tool.create(name: "Tool4",
+Tool.create!(name: "Tool4",
   description: description.sample,
   category: category.sample,
   available: true,
   user: User.first,
   location: location.sample,
-  photo: photo.sample,
   price_per_day: price_per_day.sample)
 
-Tool.create(name: "Tool4",
+Tool.create!(name: "Tool4",
   description: description.sample,
   category: category.sample,
   available: true,
   user: User.first,
   location: location.sample,
-  photo: photo.sample,
   price_per_day: price_per_day.sample)
