d532b337938026615de3ca5881cbcbe92fa7d11a
Lucas Petti <luc_petti@hotmail.com>
2018-11-23 12:29:08 +0100

    Updated search options

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,10 +3,10 @@ class PagesController < ApplicationController
 
   def home
     # @locations = Tool.pluck(:location).uniq
-    @locations = ["Berlin", "Munich", "Paris", "Düsseldorf"]
+    @locations = ["Berlin", "Düsseldorf", "Munich"]
     # tools_loc = Tool.where(location: params[:location])
     # tools_cat = Tool.where(category: params[:category])
     # @tools = (tools_cat + tools_loc).uniq
-    @tools = Tool.pluck(:category).uniq
+    @tools = ["Chainsaw", "Drill", "Grinder", "Hammer"]
   end
 end
